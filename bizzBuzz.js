12bd3ae34b25e13470be722b329d9b8c7920c061
KAkos27 <akoskosztolanyi@gmail.com>
2024-04-11 15:22:25 +0200

    sda

--- bizzBuzz.js
+++ bizzBuzz.js
@@ -1,3 +1,30 @@
-const bizzBuzz = (n) => {};
+const seged = (n) => {
+  let eredmeny;
+  if (n === 1) {
+    eredmeny = "1";
+  } else if (n % 2 === 0 && n % 3 === 0) {
+    eredmeny = "BIZZBUZZ";
+  } else if (n % 2 === 0) {
+    eredmeny = "BIZZ";
+  } else if (n % 3 === 0) {
+    eredmeny = "BUZZ";
+  } else {
+    eredmeny = n;
+  }
+
+  return eredmeny;
+};
+
+const bizzBuzz = (n) => {
+  let eredmeny;
+  if (n <= 0) {
+    eredmeny = "nincs ilyen sorozat";
+  } else {
+    for (let i = 0; i <= n; i++) {
+      eredmeny += i === n ? seged(i) : seged(i) + ", ";
+    }
+  }
+  return eredmeny;
+};
 
 export default bizzBuzz;
--- tesztBizzBuzz.js
+++ tesztBizzBuzz.js
@@ -1,5 +1,37 @@
 import bizzBuzz from "./bizzbuzz.js";
 
-const teszBizzBuzz = () => {};
+const teszBizzBuzz = () => {
+  const tesztEsetek = [
+    {
+      n: 0,
+      vart: "nincs ilyen sorozat",
+    },
+    {
+      n: -1,
+      vart: "nincs ilyen sorozat",
+    },
+    {
+      n: 1,
+      vart: "1",
+    },
+    {
+      n: 2,
+      vart: "1, BIZZ",
+    },
+    {
+      n: 3,
+      vart: "1, BIZZ, BUZZ",
+    },
+  ];
+
+  tesztEsetek.forEach((tesztEset) => {
+    const kapott = bizzBuzz(tesztEset.n);
+    console.assert(
+      tesztEset.vart === kapott,
+      "%o",
+      `n: ${tesztEset.n} | várt: ${tesztEset.vart} | kapott: ${kapott}`
+    );
+  });
+};
 
 export default teszBizzBuzz;
